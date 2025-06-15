4945564de9b6e35bc77368efecab2759eaa2327f
salah2eddin <salaheddin.mohamed12345@gmail.com>
2025-06-15 19:44:04 +0300

    feat: otp_service.py with all otp generation, sending, and verification logic.

--- service/auth_service.py
+++ service/auth_service.py
@@ -1,35 +1,18 @@
-import random
-from utils.email import send_otp_email
 from schemas.user_schema import UserCreate, NewUser
-from schemas.otp_schema import OTPVerify
 from passlib.context import CryptContext
 from passlib.exc import UnknownHashError
-from crud.user_crud import create_user, verify_user
-from crud.otp_crud import create_otp, get_otp, delete_otp
+from crud.user_crud import create_user
+from service.otp_service import send_otp
 
 
-def initiate_signup(user_data: UserCreate) -> str:
+def initiate_signup(user_data: UserCreate):
     hashed_user = NewUser(
         email=user_data.email,
         username=user_data.username,
         password=hash_password(user_data.password.get_secret_value())
     )
     create_user(hashed_user)
-
-    otp = str(random.randint(100000, 999999))
-    create_otp(user_data.email, otp)
-
-    send_otp_email(user_data.email, otp)
-    return otp
-
-
-def verify_otp(data: OTPVerify) -> bool:
-    saved_otp = get_otp(data.email)
-    if saved_otp and saved_otp == data.password:
-        verify_user(data.email)
-        delete_otp(data.email)
-        return True
-    return False
+    send_otp(user_data.email)
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
