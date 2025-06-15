7bb2ae54d82f065b352e8a2d6cab3993f673e759
salah2eddin <salaheddin.mohamed12345@gmail.com>
2025-06-15 18:55:51 +0300

    feat: initiate_signup and verify_otp use crud operations in otp_crud.py

--- service/auth_service.py
+++ service/auth_service.py
@@ -2,15 +2,10 @@ import random
 from utils.email import send_otp_email
 from schemas.user_schema import UserCreate, NewUser
 from schemas.otp_schema import OTPVerify
-from database import database
 from passlib.context import CryptContext
 from passlib.exc import UnknownHashError
 from crud.user_crud import create_user, verify_user
-
-otp_collection = database.get_collection("otp")
-
-
-# service/auth_service.py (or wherever you keep your service logic)
+from crud.otp_crud import create_otp, get_otp, delete_otp
 
 
 def initiate_signup(user_data: UserCreate):
@@ -22,26 +17,27 @@ def initiate_signup(user_data: UserCreate):
     create_user(hashed_user)
 
     otp = str(random.randint(100000, 999999))
-    otp_collection.insert_one({
-        "email": user_data.email,
-        "otp": otp})
+    create_otp(user_data.email, otp)
 
     send_otp_email(user_data.email, otp)
     return otp
 
+
 def verify_otp(data: OTPVerify):
-    record = otp_collection.find_one({"email": data.email, "otp": data.otp})
-    if record:
+    saved_otp = get_otp(data.email)
+    if saved_otp and saved_otp == data.password:
         verify_user(data.email)
-        otp_collection.delete_one({"_id": record["_id"]})
+        delete_otp(data.email)
         return True
     return False
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
+
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     # print("Hashed password from DB:", hashed_password)
