25e389aa2b46ce7e8cbaadba711315032e6a4e0c
salah2eddin <salaheddin.mohamed12345@gmail.com>
2025-06-15 20:04:40 +0300

    feat: check_user_exists function used in making sure users are unique

--- service/auth_service.py
+++ service/auth_service.py
@@ -1,10 +1,25 @@
 from schemas.user_schema import UserCreate, NewUser
 from utils.auth import hash_password
 from crud.user_crud import create_user
+from fastapi import HTTPException, status
+from crud.user_crud import create_user, get_user_by_email
 from service.otp_service import send_otp
 
 
+def check_user_exists(email: str):
+    existing_user = get_user_by_email(email)
+    if existing_user:
+        return True
+    return False
+
+
 def initiate_signup(user_data: UserCreate):
+    if check_user_exists(user_data.email):
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Email is already registered"
+        )
+
     hashed_user = NewUser(
         email=user_data.email,
         username=user_data.username,
