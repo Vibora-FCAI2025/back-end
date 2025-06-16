da71d4de8cae2b9afa2ca85a9eca27529ae89858
salah2eddin <salaheddin.mohamed12345@gmail.com>
2025-06-16 05:16:49 +0300

    feat: added match router to app

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from controller.auth_controller import router as auth_router
+from controller.match_controller import router as match_router
 
 app = FastAPI()
 
 app.include_router(auth_router, prefix="/auth", tags=["UserAPI"])
+app.include_router(match_router, prefix="/analysis", tags=["MatchAPI"])
