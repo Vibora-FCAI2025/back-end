036781154234d3047be38117b29da87a2cc33bc7
salah2eddin <salaheddin.mohamed12345@gmail.com>
2025-06-23 05:51:36 +0300

    fix: renamed match parameter to analysis

--- service/analysis_service.py
+++ service/analysis_service.py
@@ -14,17 +14,17 @@ from utils.jwt import create_access_token
 settings = get_settings()
 
 
-def analyze_match(match: MatchAnalysisRequest, user: User):
+def analyze_match(analysis: MatchAnalysisRequest, user: User):
     match_create = MatchCreate(
-        video_id=ObjectId(match.video_id),
+        video_id=ObjectId(analysis.video_id),
         user_id=user.id,
         date=datetime.now(),
-        video_url=generate_download_url(match.video_id)
+        video_url=generate_download_url(analysis.video_id)
     )
 
     match_id = create_match(match_create)
     match = get_match_by_id(match_id)
-    send_analysis_request(match, match.keypoints)
+    send_analysis_request(match, analysis.keypoints)
 
     return match_id
 
